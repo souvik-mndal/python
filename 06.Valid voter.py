c17a0106866b64655aeb8c6eeb2a04ea10fac5a2
souvik mndal <souvkmndal@gmail.com>
2025-09-03 17:24:35 +0530

    some question on conditionals...

---
+++ 06.Valid voter.py
@@ -0,0 +1,7 @@
+name = input("Enter the name : ")
+age = int(input("Enter the age : ")
+)
+if age>=18 :
+    print(f"the user {name} is a valid voter")
+else:
+    print(f"the user {name} is a not valid voter")
