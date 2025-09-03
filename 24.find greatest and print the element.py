31d93df042bfdc1866072ce1773b630091eeb297
souvik mndal <souvkmndal@gmail.com>
2025-09-04 00:47:31 +0530

    some questions on list....

--- 24.find greatest and print the element.py
+++ 24.find greatest and print the element.py
@@ -1,9 +1,9 @@
 lst = [16,4,1,4,6,658,4,65,6,4,231,5,1,31,35,84,6,6,4,153,48,531,564]
 
-greatest = -1
-index = -1
+greatest = lst[0]
+index = 0
 
-for i in range(0,len(lst)):
+for i in range(1,len(lst)):
     if( lst[i] > greatest ):
         greatest = lst[i]
         index = i
