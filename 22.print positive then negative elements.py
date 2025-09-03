4fc50fb13548410387865857b52ae3586bd782c1
souvik mndal <souvkmndal@gmail.com>
2025-09-03 23:36:48 +0530

    some question on list...

---
+++ 22.print positive then negative elements.py
@@ -0,0 +1,13 @@
+lst = [ -59,489,-654,45,4,-864,-64,-654,654,46,-64,4]
+
+# lst.sort()
+print("positive")
+for i in lst:
+    if( i>=0 ):
+        print(i)
+print("Negative")
+for i in lst:
+    if( i<0 ):
+        print(i)
+
+
