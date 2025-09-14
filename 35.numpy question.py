c64c3242f9e6b1ea5d30fca71d78c421cfb4b1c3
souvik mndal <souvkmndal@gmail.com>
2025-09-14 20:54:28 +0530

    numpy question

---
+++ 35.numpy question.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# age , math marks , science marks
+data = np.array([
+    [18, 85, 78],  # Student 1
+    [19, 92, 88],  # Student 2
+    [17, 76, 95],  # Student 3
+    [18, 65, 70],  # Student 4
+    [20, 90, 85]   # Student 5
+])
+
+
+#To get the shape of the matrix
+print(f"the shape of the matrix is --- {np.shape(data)}")
+
