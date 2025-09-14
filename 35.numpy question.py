02ce22850200cf4703bb16c1a8c85dee7dff9764
souvik mndal <souvkmndal@gmail.com>
2025-09-14 21:10:54 +0530

    numpy question

--- 35.numpy question.py
+++ 35.numpy question.py
@@ -15,8 +15,7 @@ print(f"the shape of the matrix is --- {np.shape(data)}")
 
 #To get the average age of the student
 age = data[: , 0:1 ]
-avg = age.sum() / len(age)
-print(f"the average age of the student are --- {avg}")
+print(f"the average age of the student are --- {np.mean(age)}")
 
 
 #To extract maths marks from all student 
@@ -27,5 +26,13 @@ print(f"the maths marks for all student are --- {np.vsplit(math,len(math))}")
 #To find highest science marks from all student 
 science = data[: , 2:]
 print(f"the highest science marks for all student are --- {science.max()}")
+
+
+#To get the details of the student who scored more than 90 in maths
+math = data[: , 1:2]
+boolcheck = math > 90
+for i in range( len(boolcheck) ):
+    if( boolcheck[i] ):
+        print(f"the data of the student who scored more than 90 in maths are ---\n {data[i]}")
 
 
